7abb3d0e6c420e9ac193c98e1c5a27a10535f510
Maximiliano-Pereyra <maxigenio99@gmail.com>
2022-10-06 14:20:32 -0300

    primera tanda

---
+++ main.js
@@ -0,0 +1,143 @@
+/* let miNombre;
+miNombre = "Maxi";
+console.log(miNombre);
+
+let miApellido;
+miApellido = "Pereyra";
+console.log(miApellido);
+
+let miEdad;
+miEdad = 23;
+console.log(miEdad);
+
+let miMascota;
+miMascota = "Apolo";
+console.log(miMascota);
+
+let edadMascota;
+edadMascota = 3;
+console.log(edadMascota);
+ */
+/* let nombreCompleto;
+nombreCompleto = miNombre + " " + miApellido
+console.log(nombreCompleto);
+
+let textoDePresentacion;
+textoDePresentacion ="Hola, mi nombre es " + miNombre + " " + miApellido + " tengo " + miEdad + " años y un perro que se llama " + miMascota + " , el cual tiene " + edadMascota + "de edad"
+console.log(textoDePresentacion);
+
+let sumaEdades;
+sumaEdades = miEdad + edadMascota;
+console.log(sumaEdades);
+
+let restaEdades;
+restaEdades = miEdad - edadMascota;
+console.log(restaEdades);
+
+let productoEdades;
+productoEdades = miEdad * edadMascota;
+console.log(productoEdades);
+
+let divisionEdades;
+divisionEdades = miEdad / edadMascota;
+console.log(divisionEdades); */
+
+/* let miNombre;
+miNombre = prompt( 'Ingrese su nombre' );
+console.log(miNombre);
+
+let miApellido;
+miApellido = prompt( 'Ingrese su apellido' );
+console.log(miApellido);
+
+let miEdad;
+ miEdad = prompt( 'Ingrese su edad' );
+console.log(miEdad);
+
+let miMascota;
+miMascota = prompt( 'Ingrese el nombre de su mascota' );
+console.log(miMascota);
+
+let edadMascota ;
+ edadMascota = prompt( 'Ingrese la edad de su mascota' );
+console.log(edadMascota);
+
+let nombreCompleto;
+nombreCompleto = miNombre + " " + miApellido
+console.log(nombreCompleto);
+
+let textoDePresentacion;
+textoDePresentacion = "Hola, mi nombre es " + miNombre + " " + miApellido + " tengo " + miEdad + " años y un perro que se llama " + miMascota + " , el cual tiene " + edadMascota + "de edad"
+console.log(textoDePresentacion);
+
+let sumaEdades;
+sumaEdades = miEdad + edadMascota;
+console.log(sumaEdades);
+
+let restaEdades;
+restaEdades = miEdad - edadMascota;
+console.log(restaEdades);
+
+let productoEdades;
+productoEdades = miEdad * edadMascota;
+console.log(productoEdades);
+
+let divisionEdades;
+divisionEdades = miEdad / edadMascota;
+console.log(divisionEdades); */
+
+/* let alumno = {
+    nombre: "Maxi",
+    edad: 23,
+    altura: 1.76,
+    sexo: "Masculino",
+    peligroso: false,
+}
+console.log(alumno);
+console.log(alumno.nombre);
+console.log(alumno.edad);
+console.log(alumno.altura);
+console.log(alumno.sexo);
+console.log(alumno.peligroso);
+
+let mascota = {
+    nombre: "Apolo",
+    edad: 3,
+    color: "Negro",
+    sexo: "Masculino",
+    peligroso: false,
+}
+console.log(mascota);
+console.log(mascota.nombre);
+console.log(mascota.edad);
+console.log(mascota.color);
+console.log(mascota.sexo);
+console.log(mascota.peligroso);
+
+let frutas = ["banana", "manzana", "pera", "tomate", "durazno"]
+console.log(frutas);
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+console.log(frutas[4]);
+
+let numeros = [1, 3, 5, 7, 9]
+console.log(numeros);
+console.log(numeros[0]);
+console.log(numeros[1]);
+console.log(numeros[2]);
+console.log(numeros[3]);
+console.log(numeros[4]);
+
+let familia = [{nombre:"Papa", edad:49},{nombre:"Mama", edad:50},{nombre:"Hermano mayor", edad:20}, {nombre:"Hermano menor", edad:16}, {nombre:"Mascota", edad:3}]
+console.log(familia);
+console.log(familia[0].nombre);
+console.log(familia[1].nombre);
+console.log(familia[2].nombre);
+console.log(familia[3].nombre);
+console.log(familia[4].nombre);
+
+let textoAleatorio = "A mi " + (familia[4]) + " le gusta mucho la " + (frutas[1]) + " sin envargo no se come mas de " + (numeros[3]) + " por dia."
+console.log(textoAleatorio); */
+
